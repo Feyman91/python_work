a8f6614660b963bfb7d2a3478546cfca6ba2c9d7
richard_syh <richard_syh@foxmail.com>
2023-08-30 20:19:07 +0800

    create multiply_func

--- add_func.py
+++
@@ -1,4 +0,0 @@
-#this is a testA for git cooperation funtion
-#aim to design add funtion
-def add(input1,input2):
-    return input1+input2
---
+++ multiply_func.py
@@ -0,0 +1,4 @@
+#this is a testB for git cooperation funtion
+#aim to design multiply funtion
+def multiply(input1,input2):
+    return input1*input2
