7b434f94bb2b52e2a9b4efaea3a4eaf17f9bffa1
syh <useremail@666.com>
2023-08-30 19:48:57 +0800

    initiation

---
+++ main.py
@@ -0,0 +1,5 @@
+#this is a test for git cooperation funtion
+#we aim to design an add&mutiply main funtion from two users:testA, testB
+#testA design add funtion while testB design multiply funtion
+a = 0
+b =0
